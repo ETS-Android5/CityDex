3b84e7a3a6237c6ad564b5ff4565187bd6af6878
TLBail <62939298+TLBail@users.noreply.github.com>
2021-10-23 18:19:08 +0200

    stop

--- app/src/main/java/com/example/ptuts3androidapp/View/MainActivity.java
+++ app/src/main/java/com/example/ptuts3androidapp/View/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.ptuts3androidapp.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.ImageView;
 
 import com.example.ptuts3androidapp.R;
 
@@ -12,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),  R.drawable.ic_cow_background);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageBitmap(bitmap);
     }
 }
