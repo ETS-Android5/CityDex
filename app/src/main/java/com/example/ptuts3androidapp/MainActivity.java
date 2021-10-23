d203f64267ca6d34cb18ac159844ac04191eec89
Nathan POLLART <nathan.pollart@hotmail.fr>
2021-10-23 18:24:55 +0200

    delete gitignore

--- app/src/main/java/com/example/ptuts3androidapp/MainActivity.java
+++ app/src/main/java/com/example/ptuts3androidapp/MainActivity.java
@@ -1,25 +1,11 @@
 package com.example.ptuts3androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.googlecode.tesseract.android.TessBaseAPI;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         TessOCR ocr = new TessOCR();
         String text = ocr.getOCRResult(((BitmapDrawable)imgView.getDrawable()).getBitmap());
-
+        textView.setText(text);
 
     }
 }
