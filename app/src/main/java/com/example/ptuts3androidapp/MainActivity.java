6b8d0d27a2d952028b9806744bdb58ce54dad208
Nathan POLLART <nathan.pollart@hotmail.fr>
2021-10-23 18:08:46 +0200

    OCR start

--- app/src/main/java/com/example/ptuts3androidapp/MainActivity.java
+++ app/src/main/java/com/example/ptuts3androidapp/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.ptuts3androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgView;
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        TessOCR ocr = new TessOCR();
+        String text = ocr.getOCRResult(((BitmapDrawable)imgView.getDrawable()).getBitmap());
+
+
     }
 }
