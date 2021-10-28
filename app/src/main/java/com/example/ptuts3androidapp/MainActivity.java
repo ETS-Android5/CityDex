737cc8766a259d5093a732f5b1dbf96fb44f967f
Almaaik <axelgueranger@gmail.com>
2021-10-28 14:47:29 +0200

    initialisation des composants graphiques dans le main

--- app/src/main/java/com/example/ptuts3androidapp/MainActivity.java
+++ app/src/main/java/com/example/ptuts3androidapp/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.ptuts3androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    //propiétés
+    private ImageButton collec_button;
+    private ImageButton succes_button;
+    private ImageButton reglage_button;
+    private ImageButton photo_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Initialisation
+     */
+    private void initActivity(){
+        //lien vers les objets graphiques
+        collec_button = (ImageButton) findViewById(R.id.collec_button);
+        succes_button = (ImageButton) findViewById(R.id.succes_button);
+        reglage_button = (ImageButton) findViewById(R.id.reglage_button);
+        photo_button = (ImageButton) findViewById(R.id.photo_button);
+
     }
 }
