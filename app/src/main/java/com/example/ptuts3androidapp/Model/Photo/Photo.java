6e5781267d2cfb53df7730e7075d13b79ef24b86
TLBail <62939298+TLBail@users.noreply.github.com>
2021-11-05 17:59:16 +0100

    merge data persistance branch to main branch

---
+++ app/src/main/java/com/example/ptuts3androidapp/Model/Photo/Photo.java
@@ -0,0 +1,37 @@
+package com.example.ptuts3androidapp.Model.Photo;
+
+import android.net.Uri;
+
+import java.io.Serializable;
+
+public class Photo implements Serializable {
+
+    private String photoUri;
+    private String location; //TODO choisir le bon type pour le stockage de la postion
+
+    public Photo(String photoPath, String location) {
+        this(Uri.parse(photoPath), location);
+    }
+
+    public Photo(Uri photoUri, String location) {
+        this.photoUri = photoUri.toString();
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+
+    public Uri getPhotoUri() {
+        return Uri.parse(photoUri);
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "photoUri='" + photoUri + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/ptuts3androidapp/Model/Photo/PhotoTaker.java
@@ -0,0 +1,121 @@
+package com.example.ptuts3androidapp.Model.Photo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.FileProvider;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.example.ptuts3androidapp.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PhotoTaker {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private AppCompatActivity appCompatActivity;
+
+    String currentPhotoPath;
+    private ImageView imageView;
+    private ActivityResultLauncher<Intent> mStartForResult;
+    public Uri photoUri;
+
+
+    public PhotoTaker(AppCompatActivity appCompatActivity) {
+        this.appCompatActivity = appCompatActivity;
+        imageView = appCompatActivity.findViewById(R.id.imageView);
+        mStartForResult = appCompatActivity.registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent intent = result.getData();
+                        }
+                    }
+                });
+    }
+
+    public void takePhoto(){
+        dispatchTakePictureIntent();
+        galleryAddPic();
+    }
+
+
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        appCompatActivity.sendBroadcast(mediaScanIntent);
+        photoUri = contentUri;
+
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(appCompatActivity.getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(appCompatActivity,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                mStartForResult.launch(takePictureIntent);
+
+                //appCompatActivity.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = appCompatActivity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+
+    }
+
+
+
+}
