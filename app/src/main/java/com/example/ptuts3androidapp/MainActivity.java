d4188168f66327af84c52c66b7d6e12a735d012d
Almaajik <axelgueranger@gmail.com>
2021-11-09 09:55:22 +0100

    id

--- app/src/main/java/com/example/ptuts3androidapp/MainActivity.java
+++ app/src/main/java/com/example/ptuts3androidapp/MainActivity.java
@@ -2,16 +2,9 @@ package com.example.ptuts3androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
     }
 
     /**
