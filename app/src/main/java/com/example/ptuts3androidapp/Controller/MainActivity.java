d8a27d1471439bb271a096b0d994cb717c1a0cc0
TLBail <62939298+TLBail@users.noreply.github.com>
2021-11-09 10:50:47 +0100

    display home activity

--- app/src/main/java/com/example/ptuts3androidapp/Controller/MainActivity.java
+++ app/src/main/java/com/example/ptuts3androidapp/Controller/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.ptuts3androidapp.R;
+import com.example.ptuts3androidapp.View.BackgroundOfPhoto.BackgroundRecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
+
+        new BackgroundRecyclerView(this, findViewById(R.id.recyclerView));
+
+
     }
 }
