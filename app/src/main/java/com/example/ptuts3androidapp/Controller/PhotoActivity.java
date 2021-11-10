981804f88f2d19bf9ba3a4e3a6b693078290f079
TLBail <62939298+TLBail@users.noreply.github.com>
2021-11-10 15:40:42 +0100

    add frag

--- app/src/main/java/com/example/ptuts3androidapp/Controller/PhotoActivity.java
+++ app/src/main/java/com/example/ptuts3androidapp/Controller/PhotoActivity.java
@@ -11,6 +11,6 @@ public class PhotoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.photo);
+        setContentView(R.layout.activity_photo);
     }
 }
