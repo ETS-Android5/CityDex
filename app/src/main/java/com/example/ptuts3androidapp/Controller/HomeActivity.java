230630ad97ee6532cadcd78fc186fd92ccd96924
TLBail <62939298+TLBail@users.noreply.github.com>
2021-11-11 21:16:34 +0100

    fix github action ?

--- app/src/main/java/com/example/ptuts3androidapp/Controller/HomeActivity.java
+++ app/src/main/java/com/example/ptuts3androidapp/Controller/HomeActivity.java
@@ -24,7 +24,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        backgroundRecyclerView.start(findViewById(R.id.recyclerView));
+        backgroundRecyclerView.start(findViewById(R.id.homeRecyclerView));
     }
 
 
