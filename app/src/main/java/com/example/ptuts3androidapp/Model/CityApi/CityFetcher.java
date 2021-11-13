f58bae83da5ff0aa801e0c7d2b2bc85ee0d76047
TLBail <62939298+TLBail@users.noreply.github.com>
2021-11-13 22:27:28 +0100

    add city fecthing

---
+++ app/src/main/java/com/example/ptuts3androidapp/Model/CityApi/CityFetcher.java
@@ -0,0 +1,12 @@
+package com.example.ptuts3androidapp.Model.CityApi;
+
+import java.util.List;
+
+public interface CityFetcher {
+
+	City getCity(String name, Department department);
+	List<City> getCitiesByDepartment(Department department);
+	List<City> getCitiesBySurface(float surface);
+	List<City> getCitiesByName(String name);
+	List<City> getCitiesByInhabitance(int nbPeople);
+}
