e0d4c324e687b6955e5b49e9b0e3a8a57d88053c
Suna24 <bastien.t2002@gmail.com>
2021-11-18 13:14:06 +0100

    correction des noms des villes and départements

--- app/src/main/java/com/example/ptuts3androidapp/Model/CityApi/Region.java
+++ app/src/main/java/com/example/ptuts3androidapp/Model/CityApi/Region.java
@@ -1,23 +1,23 @@
 package com.example.ptuts3androidapp.Model.CityApi;
 
 public enum Region {
-	AUVERGNE_RHONES_ALPES("Auvergne-Rh�ne-Alpes"),
-	BOURGOGNE_FRANCHE_COMTE("Bourgogne-Franche-Comt�"),
+	AUVERGNE_RHONES_ALPES("Auvergne-Rhône-Alpes"),
+	BOURGOGNE_FRANCHE_COMTE("Bourgogne-Franche-Comté"),
 	BRETAGNE("Bretagne"),
 	CENTRE_VAL_DE_LOIRE("Centre-Val de Loire"),
 	CORSE("Corse"),
 	GRAND_EST("Grand Est"),
 	HAUTS_DE_FRANCE("Hauts-de-France"),
-	ILE_DE_FRANCE("�le-de-France"),
+	ILE_DE_FRANCE("Île-de-France"),
 	NORMANDIE("Normandie"),
 	NOUVELLE_AQUITAINE("Nouvelle-Aquitaine"),
 	OCCITANIE("Occitanie"),
 	PAYS_DE_LA_LOIRE("Pays de la Loire"),
-	PROVENCE_ALPES_COTE_D_AZUR("Provence-Alpes-C�te d'Azur"),
+	PROVENCE_ALPES_COTE_D_AZUR("Provence-Alpes-Côte d'Azur"),
 	GUADELOUPE("Guadeloupe"),
 	MARTINIQUE("Martinique"),
 	GUYANE("Guyane"),
-	LA_REUNION("La R�union"),
+	LA_REUNION("La Réunion"),
 	MAYOTTE("Mayotte");
 	
 	
