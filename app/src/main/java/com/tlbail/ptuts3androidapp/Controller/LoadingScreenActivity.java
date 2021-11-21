c10dc8ab5e77af47919be30368133efc66a280fc
TLBail <62939298+TLBail@users.noreply.github.com>
2021-11-21 15:19:39 +0100

    first iteration succès

--- app/src/main/java/com/tlbail/ptuts3androidapp/Controller/LoadingScreenActivity.java
+++ app/src/main/java/com/tlbail/ptuts3androidapp/Controller/LoadingScreenActivity.java
@@ -1,11 +1,21 @@
 package com.tlbail.ptuts3androidapp.Controller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.games.AchievementsClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.tlbail.ptuts3androidapp.LoadingAnimation;
 import com.example.ptuts3androidapp.R;
 
@@ -15,7 +25,12 @@ public class LoadingScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        setupAchievement();
+
         LoadingAnimation loadingAnimation = new LoadingAnimation(findViewById(R.id.loading_screen), 4);
+
+
         loadingAnimation.start();
         loadingAnimation.getAnimation().addListener(new Animator.AnimatorListener() {
             @Override
@@ -39,6 +54,48 @@ public class LoadingScreenActivity extends AppCompatActivity {
 
             }
         });
+
+
+
+    }
+
+    private void setupAchievement() {
+        GoogleSignInOptions  signInOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+                        .requestScopes(Games.SCOPE_GAMES_SNAPSHOTS)
+                        .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this,signInOptions);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (GoogleSignIn.hasPermissions(account, signInOptions.getScopeArray())) {
+            // Already signed in.
+            // The signed in account is stored in the 'account' variable.
+            GoogleSignInAccount signedInAccount = account;
+            AchievementsClient achievementsClient = Games.getAchievementsClient(getApplicationContext(), signedInAccount);
+            Log.i("achivement", achievementsClient.toString());
+        } else {
+            // Haven't been signed-in before. Try the silent sign-in first.
+            GoogleSignInClient signInClient = GoogleSignIn.getClient(this, signInOptions);
+            signInClient
+                    .silentSignIn()
+                    .addOnCompleteListener(
+                            this,
+                            new OnCompleteListener<GoogleSignInAccount>() {
+                                @Override
+                                public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                                    if (task.isSuccessful()) {
+                                        // The signed in account is stored in the task's result.
+                                        GoogleSignInAccount signedInAccount = task.getResult();
+                                        AchievementsClient achievementsClient = Games.getAchievementsClient(getApplicationContext(), signedInAccount);
+                                        Log.i("achievementsClient = " , achievementsClient.toString());
+                                    } else {
+                                        // Player will need to sign-in explicitly using via UI.
+                                        // See [sign-in best practices](http://developers.google.com/games/services/checklist) for guidance on how and when to implement Interactive Sign-in,
+                                        // and [Performing Interactive Sign-in](http://developers.google.com/games/services/android/signin#performing_interactive_sign-in) for details on how to implement
+                                        // Interactive Sign-in.
+                                    }
+                                }
+                            });
+        }
 
 
 
