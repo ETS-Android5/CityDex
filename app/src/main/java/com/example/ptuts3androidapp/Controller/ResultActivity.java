a3935267e0278b5bebb9e48928ff64b36b4cab94
Suna24 <bastien.t2002@gmail.com>
2021-11-24 18:54:48 +0100

    Réorganisation du code et demande à l'utilisateur d'activer sa localisation le cas échéant

--- app/src/main/java/com/example/ptuts3androidapp/Controller/ResultActivity.java
+++ app/src/main/java/com/example/ptuts3androidapp/Controller/ResultActivity.java
@@ -25,36 +25,27 @@ import android.widget.Toast;
 import com.example.ptuts3androidapp.Model.DetectionTextPanneau.OCRFromObjectDetector;
 import com.example.ptuts3androidapp.Model.DetectionTextPanneau.OnPanneauResultFinishListener;
 import com.example.ptuts3androidapp.Model.DetectionTextPanneau.ResultScan;
+import com.example.ptuts3androidapp.Model.Localisation.LocalisationManager;
+import com.example.ptuts3androidapp.Model.Localisation.OnLocationFind;
 import com.example.ptuts3androidapp.R;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
-public class ResultActivity extends AppCompatActivity implements OnPanneauResultFinishListener, LocationListener {
-
-    private LocationManager locationManager;
-    private static final String[] LOCATION_PERMS = {Manifest.permission.ACCESS_FINE_LOCATION};
-    private static final int LOCATION_REQUEST = 1340;
+public class ResultActivity extends AppCompatActivity implements OnPanneauResultFinishListener, OnLocationFind {
 
     private Bitmap bitmapPhoto;
     private ImageView imageView;
     private OCRFromObjectDetector ocrFromObjectDetector;
     private TextView textView;
-    private StringBuilder msg;
+    private LocalisationManager localisationManager;
 
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getDataFromPhotoActivity();
         setContentView(R.layout.activity_result);
         bindUI();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(LOCATION_PERMS, LOCATION_REQUEST);
-        }
+        startLocation();
     }
 
     private void getDataFromPhotoActivity() {
@@ -88,13 +79,8 @@ public class ResultActivity extends AppCompatActivity implements OnPanneauResult
         super.onResume();
         imageView.setImageBitmap(bitmapPhoto);
         startOCR();
-
-        //Obtention de la référence du service
-        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-
-        //Si le GPS est disponible, on s'y abonne
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            abonnementGPS();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            localisationManager.onResumeActivity();
         }
     }
 
@@ -102,13 +88,17 @@ public class ResultActivity extends AppCompatActivity implements OnPanneauResult
     public void onPause() {
         super.onPause();
 
-        //On appelle la méthode pour se désabonner
-        desabonnementGPS();
+        localisationManager.desabonnementGPS();
     }
 
     private void startOCR() {
         ocrFromObjectDetector = new OCRFromObjectDetector(getApplicationContext());
         ocrFromObjectDetector.runObjetDetectionAndOCR(bitmapPhoto, this);
+    }
+
+    private void startLocation(){
+        localisationManager = new LocalisationManager(this);
+        localisationManager.requestPermission();
     }
 
     @Override
@@ -122,70 +112,14 @@ public class ResultActivity extends AppCompatActivity implements OnPanneauResult
         });
     }
 
-    /**
-     * Méthode permettant de s'abonner à la localisation par GPS.
-     */
-    public void abonnementGPS() {
-        //On s'abonne
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
-    }
-
-    /**
-     * Méthode permettant de se désabonner de la localisation par GPS.
-     */
-    public void desabonnementGPS() {
-        //Si le GPS est disponible, on s'y abonne
-        locationManager.removeUpdates(this);
-    }
-
     @Override
-    public void onLocationChanged(final Location location) {
-        //On affiche dans un message la nouvelle Localisation
-        msg = new StringBuilder("lat : ");
-        msg.append(location.getLatitude());
-        msg.append( "; lng : ");
-        msg.append(location.getLongitude());
-
-        Log.i("Localisation", "" + msg);
-
-        Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
-        List<Address> list = null;
-        try {
-            list = geoCoder.getFromLocation(location
-                    .getLatitude(), location.getLongitude(), 1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (list != null & list.size() > 0) {
-            Address address = list.get(0);
-            String result = address.getLocality();
-            Log.i("Ville trouvée", result);
-        } else {
-            Log.i("Localisation", "Non trouvée");
-        }
-
+    public void isLocationFound(String result) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(textView.getText()
+                        + "\n Localisation trouvée : " + result);
+            }
+        });
     }
-
-    @Override
-    public void onProviderDisabled(final String provider) {
-        //Si le GPS est désactivé on se désabonne
-        if("gps".equals(provider)) {
-            desabonnementGPS();
-        }
-    }
-
-    @Override
-    public void onProviderEnabled(final String provider) {
-        //Si le GPS est activé on s'abonne
-        if("gps".equals(provider)) {
-            abonnementGPS();
-        }
-    }
-
-    @Override
-    public void onStatusChanged(final String provider, final int status, final Bundle extras) { }
-
 }
