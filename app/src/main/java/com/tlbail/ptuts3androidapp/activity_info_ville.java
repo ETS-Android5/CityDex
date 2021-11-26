d4bdb874ba563ea78032edb6ccdf7e19c06974ff
Almaajik <axelgueranger@gmail.com>
2021-11-26 11:27:37 +0100

    ajout des infos villes
    info ville
    +fix l'interface home

---
+++ app/src/main/java/com/tlbail/ptuts3androidapp/activity_info_ville.java
@@ -0,0 +1,13 @@
+package com.tlbail.ptuts3androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_info_ville extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_ville);
+    }
+}
