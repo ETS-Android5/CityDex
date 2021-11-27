bccf6efb23ed6a56ab93f1abc7fa147f14cc7d75
Almaajik <axelgueranger@gmail.com>
2021-11-27 10:44:48 +0100

    interface info ville
    interface fait reste plus qu'a faire les méthode à l'interieure pour que ce soit propre

--- app/src/main/java/com/tlbail/ptuts3androidapp/activity_info_ville.java
+++ app/src/main/java/com/tlbail/ptuts3androidapp/activity_info_ville.java
@@ -2,12 +2,18 @@ package com.tlbail.ptuts3androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class activity_info_ville extends AppCompatActivity {
+    private TextView t_ville;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_ville);
+
+
+        t_ville = findViewById(R.id.t_ville);
+        t_ville.setText("city.getCity un truc comme ça");
     }
 }
