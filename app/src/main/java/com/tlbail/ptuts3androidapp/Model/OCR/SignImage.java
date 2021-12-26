04df73d195a2205a3992512eca1d6981a4b04149
Eckaos <nathan.pollart@hotmail.fr>
2021-12-26 15:33:43 +0100

    canny

--- app/src/main/java/com/tlbail/ptuts3androidapp/Model/OCR/SignImage.java
+++ app/src/main/java/com/tlbail/ptuts3androidapp/Model/OCR/SignImage.java
@@ -5,14 +5,8 @@ import android.graphics.Bitmap;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Rect;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SignImage {
 
@@ -28,9 +22,7 @@ public class SignImage {
         Utils.bitmapToMat(src, test);
         grayscale = grayscale(test);
         cannyOutput = canny(grayscale);
-        Rect rect = getSign(cannyOutput);
-        Utils.matToBitmap(grayscale,src);
-        //src = Bitmap.createBitmap(src, (int)rect.x, (int)rect.y, (int)rect.width,(int)rect.height);
+        Utils.matToBitmap(cannyOutput,src);
         return src;
     }
 
@@ -46,40 +38,4 @@ public class SignImage {
         Imgproc.Canny(mat,cannyOutput,255/3,255);
         return cannyOutput;
     }
-
-    private Rect getSign(Mat mat){
-        Mat hierarchy = new Mat();
-        List<MatOfPoint> contours = new ArrayList<>();
-        Imgproc.findContours(mat,contours,hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-        MatOfPoint2f[] contoursPoly = new MatOfPoint2f[contours.size()];
-        Rect[] boundRect = new Rect[contours.size()];
-        for (int i = 0; i < contours.size(); i++) {
-            addRect(contours, contoursPoly, boundRect, i);
-        }
-        int i = findLargestContour(contours);
-        if(contours.size() == 0) return null;
-        return new Rect(boundRect[i].x, boundRect[i].y, boundRect[i].width, boundRect[i].height);
-    }
-
-    private void addRect(List<MatOfPoint> contours, MatOfPoint2f[] contoursPoly, Rect[] boundRect, int i) {
-        contoursPoly[i] = new MatOfPoint2f();
-        Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3 , true);
-        boundRect[i] = Imgproc.boundingRect(new MatOfPoint(new MatOfPoint(contoursPoly[i].toArray())));
-    }
-
-
-    private int findLargestContour(List<MatOfPoint> contours) {
-
-        double maxVal = 0;
-        int maxValIdx = 0;
-        for (int contourIdx = 0; contourIdx < contours.size(); contourIdx++) {
-            double contourArea = Imgproc.contourArea(contours.get(contourIdx));
-            if (maxVal < contourArea) {
-                maxVal = contourArea;
-                maxValIdx = contourIdx;
-            }
-        }
-        return maxValIdx;
-    }
-
 }
