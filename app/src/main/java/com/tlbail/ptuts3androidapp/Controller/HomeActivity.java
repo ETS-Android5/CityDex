d89cd425076501d25026dcdc9ff5a1debac33186
Suna24 <bastien.t2002@gmail.com>
2022-02-06 18:57:53 -0500

    Implémentation Notifications
    Lorsque que le joueur se trouve dans une ville qu'il n'a pas, une notification est envoyée

--- app/src/main/java/com/tlbail/ptuts3androidapp/Controller/HomeActivity.java
+++ app/src/main/java/com/tlbail/ptuts3androidapp/Controller/HomeActivity.java
@@ -10,12 +10,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.tlbail.ptuts3androidapp.Model.City.City;
+import com.tlbail.ptuts3androidapp.Model.City.CityLoaders.CityLocalLoader;
+import com.tlbail.ptuts3androidapp.Model.Localisation.LocationTrack;
+import com.tlbail.ptuts3androidapp.Model.User.LocalDataLoader.UserPropertyLocalLoader;
+import com.tlbail.ptuts3androidapp.Model.User.User;
 import com.tlbail.ptuts3androidapp.R;
 import com.tlbail.ptuts3androidapp.View.BackgroundOfPhoto.BackgroundRecyclerView;
 
+import java.io.IOException;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
@@ -24,17 +39,14 @@ public class HomeActivity extends AppCompatActivity {
 
     public static final String CHANNEL_1_ID = "channel1";
     private NotificationManagerCompat notificationManagerCompat;
+    private boolean hasCity = false;
 
     private User user;
     private List<City> cities;
 
-    private boolean hasCity = false;
-    private LocalisationManager mLocalisationManager;
-    private String adresse;
-
-    private boolean hasfinish = false;
-
-    private Context context;
+    private LocationTrack location;
+    private Geocoder geocoder;
+    private Address adresse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +55,73 @@ public class HomeActivity extends AppCompatActivity {
         backgroundRecyclerView = new BackgroundRecyclerView(this);
         setContentView(R.layout.activity_home);
         bindUI();
+
+        if(location.canGetLocation()){
+            List<Address> adresses = null;
+
+            try
+            {
+                adresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            }catch (IOException ioException)
+            {
+                Log.e("GPS", "erreur", ioException);
+            }
+            catch (IllegalArgumentException illegalArgumentException)
+            {
+                Log.e("GPS", "erreur ", illegalArgumentException);
+            }
+
+            if (adresses == null || adresses.size()  == 0)
+            {
+                Log.e("GPS", "erreur aucune adresse !");
+            }
+            else
+            {
+                adresse = adresses.get(0);
+               System.out.println(adresse);
+            }
+        }
+
+        for (City city: cities) {
+            if (city.getCityData().getName().equalsIgnoreCase(adresse.getLocality())){
+               hasCity = true;
+            }
+        }
+
+        if(!hasCity){
+            notificationManagerCompat = NotificationManagerCompat.from(this);
+            createNotif();
+        }
+    }
+
+    private void createNotif() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+
+            NotificationManager manager = this.getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+
+            sendNotif();
+        }
+    }
+
+    private void sendNotif()  {
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.mipmap.app_icon).setColor(Color.RED)
+                .setContentTitle("Nouvelle ville disponible !")
+                .setContentText("Vous vous trouvez dans une nouvelle ville ! " +
+                        "Ajouter " + adresse.getLocality() + " à votre collection !")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        int notificationId = 1;
+        this.notificationManagerCompat.notify(notificationId, notification);
     }
 
     @Override
@@ -61,6 +140,10 @@ public class HomeActivity extends AppCompatActivity {
     public void startIntent(Class className){
         Intent activityIntent = new Intent(this, className);
         this.startActivity(activityIntent);
+        user = new User(new UserPropertyLocalLoader(getApplicationContext()), new CityLocalLoader(getApplicationContext()));
+        cities = user.getOwnedCity();
+        geocoder = new Geocoder(this);
+        location = new LocationTrack(this);
     }
 
     private void startPhotoActivity() {
---
+++ app/src/main/java/com/tlbail/ptuts3androidapp/Model/Localisation/LocalisationManager.java
@@ -0,0 +1,165 @@
+package com.tlbail.ptuts3androidapp.Model.Localisation;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import com.tlbail.ptuts3androidapp.Model.PanneauVersVille.PhotoToCity;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class LocalisationManager implements LocationListener {
+
+    private LocationManager locationManager;
+    private static final String[] LOCATION_PERMS = {Manifest.permission.ACCESS_FINE_LOCATION};
+    private static final int LOCATION_REQUEST = 1340;
+    private StringBuilder msg;
+    private String locationFound;
+    private PhotoToCity photoToCity;
+    private AppCompatActivity appCompatActivity;
+
+    public LocalisationManager(PhotoToCity activity) {
+        this.photoToCity = activity;
+        this.appCompatActivity = photoToCity.getAppCompatActivity();
+    }
+
+    public LocalisationManager(AppCompatActivity appCompatActivity){
+        this.appCompatActivity = appCompatActivity;
+    }
+
+    public String getLocationFound() {
+        return locationFound;
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            appCompatActivity.requestPermissions(LOCATION_PERMS, LOCATION_REQUEST);
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void start(){
+        requestPermission();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void onResumeActivity() {
+        //Obtention de la référence du service
+        locationManager =(LocationManager) appCompatActivity.getSystemService(appCompatActivity.LOCATION_SERVICE);
+
+        //Si le GPS est disponible, on s'y abonne
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
+
+        try {
+            network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+
+        if(gps_enabled && network_enabled) {
+            abonnementGPS();
+        }
+        else {
+            Toast.makeText(appCompatActivity.getApplicationContext(), "Active INTERNET et ta LOCALISATION !", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    /**
+     * Méthode permettant de s'abonner à la localisation par GPS.
+     */
+    private void abonnementGPS() {
+        //On s'abonne
+        if (ActivityCompat.checkSelfPermission(appCompatActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(appCompatActivity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        System.out.println("start localisation..");
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
+    }
+
+    @Override
+    public void onLocationChanged(final Location location) {
+
+            //On affiche dans un message la nouvelle Localisation
+            msg = new StringBuilder("lat : ");
+            msg.append(location.getLatitude());
+            msg.append("; lng : ");
+            msg.append(location.getLongitude());
+
+            Geocoder geoCoder = new Geocoder(appCompatActivity, Locale.getDefault());
+            List<Address> list = null;
+            try {
+                list = geoCoder.getFromLocation(location
+                        .getLatitude(), location.getLongitude(), 1);
+                if (list != null & list.size() > 0) {
+                    Address address = list.get(0);
+                    locationFound = address.getLocality();
+                    msg.append("; Ville détectée : " + address.getLocality());
+
+                    Log.i("Localisation", msg + "");
+                    Log.i("VilleLocation", locationFound);
+
+                    if(photoToCity != null) {
+                        photoToCity.setLocationResult(locationFound);
+                    }
+                } else {
+                    try {
+                        throw new CityNotFoundException();
+                    } catch (CityNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
+
+    private StringBuilder toStringBuilder(){
+        return msg;
+    }
+
+    /**
+     * Méthode permettant de se désabonner de la localisation par GPS.
+     */
+    public void desabonnementGPS() {
+        //Si le GPS est disponible, on s'y abonne
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onProviderDisabled(final String provider) {
+        //Si le GPS est désactivé on se désabonne
+        if("gps".equals(provider)) {
+            desabonnementGPS();
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(final String provider) {
+        //Si le GPS est activé on s'abonne
+        if("gps".equals(provider)) {
+            abonnementGPS();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(final String provider, final int status, final Bundle extras) { }
+}
