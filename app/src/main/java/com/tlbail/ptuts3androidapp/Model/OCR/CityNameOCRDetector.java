d143f47cd11b5cf904a6ffbab729152d5d835158
Eckaos <nathan.pollart@hotmail.fr>
2022-02-08 16:43:31 -0500

    Refactor OCR class
    Rename class name
    Add a listener because of asynchronous function

--- app/src/main/java/com/tlbail/ptuts3androidapp/Model/OCR/CityNameOCRDetector.java
+++ app/src/main/java/com/tlbail/ptuts3androidapp/Model/OCR/CityNameOCRDetector.java
@@ -1,37 +1,28 @@
 package com.tlbail.ptuts3androidapp.Model.OCR;
 
-import android.graphics.Bitmap;
 import android.graphics.RectF;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.mlkit.vision.common.InputImage;
-import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 import com.tlbail.ptuts3androidapp.Model.PanneauVersVille.PhotoToCity;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
-public class OCRDetection {
+public class CityNameOCRDetector {
 
     private TextRecognizer textRecognizer;
+    private PhotoToCity photoToCity;
 
-    public OCRDetection() throws IOException {
+    public CityNameOCRDetector(PhotoToCity photoToCity){
+        this.photoToCity = photoToCity;
         textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
     }
 
-    public void runOcrResult(PhotoToCity photoToCity, RectF rectF) {
-        photoToCity.setBitmap(new SignImage().getImageForOCR(photoToCity.getBitmap(), rectF));
-        InputImage image = InputImage.fromBitmap(photoToCity.getBitmap(),0);
-        textRecognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
-            @Override
-            public void onSuccess(Text text) {
-                String result = text.getText();
-                result = correctOCRResult(result);
-                photoToCity.setOcrResult(result);
-            }
-        });
+    public void runOcrResult(RectF rectF) {
+        photoToCity.setPhotoToTransform(SignImage.getImageForOCR(photoToCity.getPhotoToTransform(), rectF));
+        InputImage image = InputImage.fromBitmap(photoToCity.getPhotoToTransform(),0);
+        textRecognizer.process(image).addOnSuccessListener(
+                text -> photoToCity.onOcrFinish(correctOCRResult(text.getText()))
+        );
     }
 
     private String correctOCRResult(String result) {
