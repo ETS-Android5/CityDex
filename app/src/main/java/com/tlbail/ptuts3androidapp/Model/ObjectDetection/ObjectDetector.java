975574cc73b22afed93775d90859b092c6cf1f12
Eckaos <nathan.pollart@hotmail.fr>
2022-02-08 17:02:34 -0500

    breaking function into smaller functions
    for readability

--- app/src/main/java/com/tlbail/ptuts3androidapp/Model/ObjectDetection/ObjectDetector.java
+++ app/src/main/java/com/tlbail/ptuts3androidapp/Model/ObjectDetection/ObjectDetector.java
@@ -5,12 +5,9 @@ import android.graphics.Bitmap;
 import android.graphics.RectF;
 import android.widget.Toast;
 
-import com.tlbail.ptuts3androidapp.Model.OCR.OcrErrorException;
-
 import org.tensorflow.lite.support.image.TensorImage;
 
 import java.io.IOException;
-import java.util.List;
 
 import com.tlbail.ptuts3androidapp.ml.Model;
 
@@ -18,57 +15,40 @@ public class ObjectDetector {
 
 
     private Context context;
+    private RectF rectLocation;
 
-    private Model.DetectionResult result;
-
-    public ObjectDetector(Context context) {
+    public ObjectDetector(Context context) {//TODO passer en singleton
         this.context = context;
     }
 
 
-    private RectF rectLocation;
-
-    public void runObjectDetection(Bitmap bitmap){
+    public void runObjectDetection(Bitmap image){
         try {
             Model model = Model.newInstance(context);
-
-
-            TensorImage image = TensorImage.fromBitmap(bitmap);
-
-            Model.Outputs outputs = model.process(image);
-
-            List<Model.DetectionResult> detectionResults = outputs.getDetectionResultList();
-            Model.DetectionResult bestResult = outputs.getDetectionResultList().get(0);
-
-            // Gets result from DetectionResult.
-            float score = bestResult.getScoreAsFloat();
-            if(score < 0.2) fail();
-
-            rectLocation = bestResult.getLocationAsRectF();
-            String objectFind = bestResult.getCategoryAsString();
-
-
-            System.out.println("objectFind = " + objectFind);
-            System.out.println("score = " + bestResult.getScoreAsFloat());
-            System.out.println("category.left = " + rectLocation.left);
-            System.out.println("category.width() = " + rectLocation.width());
-            System.out.println("category.top = " + rectLocation.top);
-            System.out.println("category.height() = " + rectLocation.height());
-
-            if(     bestResult.getLocationAsRectF().left <= 0 ||
-                    bestResult.getLocationAsRectF().top <= 0 ||
-                    bestResult.getLocationAsRectF().width() <= 0 ||
-                    bestResult.getLocationAsRectF().height() <= 0 ||
-                    bestResult.getLocationAsRectF().left + bestResult.getLocationAsRectF().width() > bitmap.getWidth() ||
-                    bestResult.getLocationAsRectF().top + bestResult.getLocationAsRectF().height() > bitmap.getHeight()
-            ) fail();
-
-            // Releases model resources if no longer used.
+            TensorImage tensorImage = TensorImage.fromBitmap(image);
+            Model.Outputs outputs = model.process(tensorImage);
+            Model.DetectionResult bestLocation = outputs.getDetectionResultList().get(0);
+            rectLocation = getLocationFromDetection(image, bestLocation);
             model.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private RectF getLocationFromDetection(Bitmap image, Model.DetectionResult bestResult) {
+        float score = bestResult.getScoreAsFloat();
+        if(score < 0.2) fail();
+        if(!isInImage(bestResult.getLocationAsRectF(), image)) fail();
+        return bestResult.getLocationAsRectF();
+    }
+
+    public boolean isInImage(RectF rectLocation, Bitmap image){
+        return  rectLocation.left > 0
+                && rectLocation.top > 0
+                && rectLocation.width() > 0
+                && rectLocation.height() > 0
+                && rectLocation.left + rectLocation.width() <= image.getWidth()
+                && rectLocation.top + rectLocation.height() <= image.getHeight();
     }
 
     private void fail(){
@@ -76,7 +56,7 @@ public class ObjectDetector {
         rectLocation = null;
     }
 
-    public RectF getRect() throws OcrErrorException {
+    public RectF getRect(){
         return rectLocation;
     }
 
