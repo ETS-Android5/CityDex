5a8c775e615dbd7807131d78b472b0ec31a3934b
Eckaos <nathan.pollart@hotmail.fr>
2022-02-08 17:00:21 -0500

    Changing these class to static class
    because it's just utility class

--- app/src/main/java/com/tlbail/ptuts3androidapp/Model/OCR/SignImage.java
+++ app/src/main/java/com/tlbail/ptuts3androidapp/Model/OCR/SignImage.java
@@ -2,7 +2,6 @@ package com.tlbail.ptuts3androidapp.Model.OCR;
 
 import android.graphics.Bitmap;
 import android.graphics.RectF;
-
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
@@ -12,10 +11,12 @@ import org.opencv.photo.Photo;
 
 public class SignImage {
 
-    public SignImage(){}
-
-    public Bitmap getImageForOCR(Bitmap src, RectF rect) {
-        src = cropImage(src, rect);
+    public static Bitmap getImageForOCR(Bitmap src, RectF rect) {
+        if(rect != null)
+            src = cropImage(src, rect);
+        return processBitmap(src);
+    }
+    private static Bitmap processBitmap(Bitmap src) {
         OpenCVLoader.initDebug();
         Mat test = new Mat();
         Utils.bitmapToMat(src, test);
@@ -25,14 +26,14 @@ public class SignImage {
         return src;
     }
 
-    private Mat grayscale(Mat mat){
+    private static Mat grayscale(Mat mat){
         Mat grayscale = new Mat();
         Imgproc.cvtColor(mat, grayscale, Imgproc.COLOR_BGR2GRAY);
         Imgproc.blur(grayscale, grayscale, new Size(3,3));
         return grayscale;
     }
 
-    private Bitmap cropImage(Bitmap src, RectF rect) {
+    private static Bitmap cropImage(Bitmap src, RectF rect) {
         Bitmap dest = src.createBitmap(src, (int) Math.max(0, rect.left), (int) rect.top, (int) rect.width(), (int) rect.height());
         return dest;
     }
