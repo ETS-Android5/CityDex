48b642d4264ddf24a4bcaa2b2d2aede546c21e66
Eckaos <nathan.pollart@hotmail.fr>
2022-02-08 21:08:36 -0500

    breaking function in smaller functions

--- app/src/main/java/com/tlbail/ptuts3androidapp/View/BackgroundOfPhoto/BackgroundRecyclerView.java
+++ app/src/main/java/com/tlbail/ptuts3androidapp/View/BackgroundOfPhoto/BackgroundRecyclerView.java
@@ -4,112 +4,91 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Handler;
 import android.provider.MediaStore;
-import android.view.MotionEvent;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSmoothScroller;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.tlbail.ptuts3androidapp.Model.City.City;
 import com.tlbail.ptuts3androidapp.Model.City.CityLoaders.CityLocalLoader;
 import com.tlbail.ptuts3androidapp.Model.User.LocalDataLoader.UserPropertyLocalLoader;
 import com.tlbail.ptuts3androidapp.Model.User.User;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BackgroundRecyclerView {
 
-    private int velocity;
+    private double velocity;
     private Context context;
-    private AppCompatActivity appCompatActivity;
-    private List<Bitmap> bitmaps;
+    private List<Bitmap> cityPhotoList;
     private RecyclerView recyclerView;
+    private Handler handler;
 
     public BackgroundRecyclerView(AppCompatActivity appCompatActivity){
-        velocity = 1;
-        this.appCompatActivity = appCompatActivity;
+        velocity = 0.5;
         this.context = appCompatActivity.getApplicationContext();
+        populateRecyclerView();
+    }
 
+    private void populateRecyclerView(){
         User user = new User(new UserPropertyLocalLoader(context) , new CityLocalLoader(context));
-
         List<City> cities = user.getOwnedCity();
-
-        bitmaps = new ArrayList<>();
-
-        for (City city: cities
-        ) {
-
-            try {
-                if(city != null && city.getPhoto() != null && city.getPhoto().getPhotoUri() != null)
-                    bitmaps.add(MediaStore.Images.Media.getBitmap(context.getContentResolver(),  city.getPhoto().getPhotoUri()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        cityPhotoList = new ArrayList<>();
+        for (City city: cities) {
+            addCityPhotoToCityPhotoList(city);
         }
+    }
 
-
-
+    private void addCityPhotoToCityPhotoList(City city){
+        try {
+            if(city != null && city.getPhoto() != null && city.getPhoto().getPhotoUri() != null)
+                cityPhotoList.add(MediaStore.Images.Media.getBitmap(context.getContentResolver(),  city.getPhoto().getPhotoUri()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
     public void start(RecyclerView recyclerView){
         this.recyclerView = recyclerView;
-        BackgroundViewAdapter backgroundViewAdapter = new BackgroundViewAdapter(bitmaps);
+        BackgroundViewAdapter backgroundViewAdapter = new BackgroundViewAdapter(cityPhotoList);
 
         recyclerView.setAdapter(backgroundViewAdapter);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
-
-        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(context){
             @Override
-            public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-                return true;
+            public void smoothScrollToPosition(RecyclerView recyclerView, RecyclerView.State state, int position) {
+                LinearSmoothScroller smoothScroller = new LinearSmoothScroller(context){
+                    @Override
+                    protected int calculateTimeForScrolling(int dx) {
+                        return dx*3;
+                    }
+                };
+                smoothScroller.setTargetPosition(position);
+                startSmoothScroll(smoothScroller);
             }
-
-            @Override
-            public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-
-            }
-
-            @Override
-            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-            }
-
         });
-
         autoScroll();
-
     }
 
-
-
     public void autoScroll() {
-        final int speedScroll = 3000;
-        Handler handler = new Handler();
         velocity = 1;
-        Runnable runnable = new Runnable() {
-            int count = -1;
+        handler.post(scrollTask());
+    }
 
+    private Runnable scrollTask(){
+        return new Runnable() {
+            int count = -1;
             @Override
             public void run() {
                 if(recyclerView.getAdapter().getItemCount() <= 0) return;
-                if (count == recyclerView.getAdapter().getItemCount() || (count == 0 && velocity == -1)) {
+                if (count+velocity < 0 || count+velocity >= recyclerView.getAdapter().getItemCount())
                     velocity = -velocity;
-                }
                 count += velocity;
                 recyclerView.smoothScrollToPosition(count);
-                handler.postDelayed(this, speedScroll);
+                handler.postDelayed(this, 10000);
             }
-
         };
-
-        handler.post(runnable);
     }
-
-
 
 }
